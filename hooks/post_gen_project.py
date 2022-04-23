24e21ec8f3111efc935e2cf814b2eb5ef84bb00e
Kevin Chu <kevin@velexi.com>
2022-04-22 19:09:51 -0700

    Add initialization of Git repository to post-generation hook.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,22 +1,24 @@
 """
-Setup steps to perform after project creation.
+Apply post-generation project configuration and setup.
 """
 # --- Imports
 
 # Standard library
+import os
 from pathlib import Path
+import subprocess
 
 
 # --- Constants
 
-PROJECT_DIRECTORY = Path.cwd()
+_PROJECT_DIRECTORY = Path.cwd()
 
 
 # --- Utility functions
 
 def _remove_file(*filepath):
     try:
-        Path(PROJECT_DIRECTORY, *filepath).unlink()
+        Path(_PROJECT_DIRECTORY, *filepath).unlink()
     except FileNotFoundError:
         pass
 
@@ -25,5 +27,13 @@ def _remove_file(*filepath):
 
 if __name__ == "__main__":
 
+    # --- Update project template files based on user configuration
+
     if "{{ cookiecutter.enable_julia }}" == "no":
         _remove_file("Project.toml")
+
+    # --- Initialize Git repository project
+
+    os.chdir(_PROJECT_DIRECTORY)
+    cmd = ["git", "init"]
+    subprocess.run(cmd, check=True)
