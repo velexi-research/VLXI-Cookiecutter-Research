01f45dc98f5a03049f246ba621e05d6a0878b23b
Kevin Chu <kevin@velexi.com>
2022-10-09 10:18:22 -0700

    Add pre-gen hook to validate Python package name.

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,15 @@
+"""
+Cookiecutter pre-generation script
+"""
+import re
+import sys
+
+
+PACKAGE_NAME_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+
+package_name = "{{ cookiecutter.package_name }}"
+
+if not re.match(PACKAGE_NAME_REGEX, package_name):
+    print()
+    print(f'ERROR: "{package_name}" is not a valid Python package name')
+    sys.exit(1)
